f7514b83a0169665de96ebeb30fc0df3de2cb7c6
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-10-02 10:52:16 +0200

    project init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 function App() {
   return (
-    <div className="bg-yellow-400 flex text-2xl font-semibold items-center justify-center p-96">
-      <h4>Hello, World!</h4>
-    </div>
+   <div>
+    
+   </div>
   );
 }
 
